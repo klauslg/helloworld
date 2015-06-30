9bb34be4fc428a7fa21da1675c55c2c712f1ae64
Klaus Lamprecht <k.lamprecht@web.de>
2015-06-30 09:42:56 +0200

    Junit ergänzt

---
+++ src/test/java/de/signaliduna/all/TestMe.java
@@ -0,0 +1,20 @@
+//add a similar class: src/test/java/de/signaliduna/all/TestMe.java/
+
+
+package de.signaliduna.all;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestMe {
+
+    @Test
+    public void aComplexComputation() {
+        String a = "a";
+        String b = "a";
+        assertEquals(a, b);
+    }
+    
+}    
+    
